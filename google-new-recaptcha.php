a1c3b29365b795aee318ebfefcac24d32b90a7be
sureshdsk <sureshdsk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-12-12 17:36:18 +0000

    version 2.0 release
    git-svn-id: https://plugins.svn.wordpress.org/are-you-robot-recaptcha/trunk@1043599 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- google-new-recaptcha.php
+++ google-new-recaptcha.php
@@ -2,12 +2,14 @@
 /*
 Plugin Name: Are you robot? google recaptcha for wordpress
 Plugin URI: http://www.idiotinside.com
-Description: Adds the new google recaptcha to wp-login page
-Version: 1.0
+Description: Adds the new google recaptcha to wp-login page, registration page, comments section and buddy press registration page.
+Version: 2.0
 Author: Suresh Kumar
 Author URI: http://profiles.wordpress.org/sureshdsk/
 */
 defined( 'ABSPATH' ) OR exit;
+
+$nocaptcha_opts = get_option('nocaptcha_login_recaptcha_options');
 function nocaptcha_login_recaptcha_page() {
     if (!current_user_can('manage_options')) {
         wp_die( __('Access denied') );
@@ -39,6 +41,52 @@ function nocaptcha_login_recaptcha_page() {
                                     <input type="text" name="nocaptcha_login_recaptcha_options[secret_key]" value="<?php echo trim($opt['secret_key']); ?>" />
                                 </td>
                             </tr>
+                            <tr valign="top">
+                                <th scope="row">Enable on Login page</th>
+                                <td>
+                                    <select name="nocaptcha_login_recaptcha_options[login]">
+                                        <option>select an option</option>
+                                        <option <?php if($opt['login'] == "1")echo "selected";?> value="1">Yes</option>
+                                        <option <?php if($opt['login'] == "0")echo "selected";?> value="0">No</option>
+                                    </select>
+
+                                </td>
+                            </tr>
+                            <tr valign="top">
+                                <th scope="row">Enable on Registration page</th>
+                                <td>
+                                    <select name="nocaptcha_login_recaptcha_options[register]">
+                                        <option>select an option</option>
+                                        <option <?php if($opt['register'] == "1")echo "selected";?> value="1">Yes</option>
+                                        <option <?php if($opt['register'] == "0")echo "selected";?> value="0">No</option>
+                                    </select>
+
+                                </td>
+                            </tr>
+                            <tr valign="top">
+                                <th scope="row">Enable on Comments</th>
+                                <td>
+                                    <select name="nocaptcha_login_recaptcha_options[comments]">
+                                        <option>select an option</option>
+                                        <option <?php if($opt['comments'] == "1")echo "selected";?> value="1">Yes</option>
+                                        <option <?php if($opt['comments'] == "0")echo "selected";?> value="0">No</option>
+                                    </select>
+
+                                </td>
+                            </tr>
+                            <tr valign="top">
+                                <th scope="row">Enable on buddypress registration</th>
+                                <td>
+                                    <select name="nocaptcha_login_recaptcha_options[buddypress]">
+                                        <option>select an option</option>
+                                        <option <?php if($opt['buddypress'] == "1")echo "selected";?> value="1">Yes</option>
+                                        <option <?php if($opt['buddypress'] == "0")echo "selected";?> value="0">No</option>
+                                    </select>
+
+                                </td>
+                            </tr>
+
+
                         </table>
                         <p class="submit"> <input type="hidden" name="process" value="1" /> <input type="submit" class="button-primary" value="Save Options" /></p>
                     </form>
@@ -59,40 +107,40 @@ function nocaptcha_login_recaptcha_page() {
 }
 
 
-    function nocaptcha_login_recaptcha_menu_pages() {
-        add_options_page('reCAPTCHA', 'reCAPTCHA', 'manage_options', 're-captcha-config', 'nocaptcha_login_recaptcha_page');
-    }
+function nocaptcha_login_recaptcha_menu_pages() {
+    add_options_page('reCAPTCHA', 'reCAPTCHA', 'manage_options', 're-captcha-config', 'nocaptcha_login_recaptcha_page');
+}
 
 
 add_action('admin_menu', 'nocaptcha_login_recaptcha_menu_pages');
 
 
 
-    function nocaptcha_login_recaptcha_form() {
-        global $recaptcha;
-        $ropt = get_option('recaptcha_options');
-        $login_recaptcha_err = 0;
+function nocaptcha_login_recaptcha_form() {
 
-        if (isset($_GET['login_recaptcha_err'])) {
-            $login_recaptcha_err = intval($_GET['login_recaptcha_err']);
-        }
 
-        $opt = get_option('nocaptcha_login_recaptcha_options');
+    $login_recaptcha_err = 0;
 
-        $captcha_code = '';
-        if ('' != $opt['site_key'] && '' != $opt['secret_key']) {
-            $captcha_code .= '<script src="https://www.google.com/recaptcha/api.js" async defer></script>
-			<div class="g-recaptcha" data-sitekey="'.htmlentities($opt['site_key']).'"></div>';
-            if (1 == $login_recaptcha_err) {
-                $captcha_code .= '<div style="color:#FF7425;">Human verification failed!</div>';
-            }
-        }
-        echo $captcha_code;
+    if (isset($_GET['login_recaptcha_err'])) {
+        $login_recaptcha_err = intval($_GET['login_recaptcha_err']);
     }
 
-add_action('login_form','nocaptcha_login_recaptcha_form');
+    $opt = get_option('nocaptcha_login_recaptcha_options');
 
+    $captcha_code = '';
+    if ('' != $opt['site_key'] && '' != $opt['secret_key']) {
+        $captcha_code .= '<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+			<div class="g-recaptcha" data-sitekey="'.htmlentities($opt['site_key']).'"></div>';
+        if (1 == $login_recaptcha_err) {
+            $captcha_code .= '<div style="color:#FF7425;">Human verification failed!</div>';
+        }
+    }
+    echo $captcha_code;
+}
 
+if($nocaptcha_opts["login"]=="1") {
+    add_action('login_form', 'nocaptcha_login_recaptcha_form');
+}
 
 if (!function_exists('nocaptcha_login_recaptcha_get_ip')) {
     function nocaptcha_login_recaptcha_get_ip() {
@@ -110,13 +158,75 @@ if (!function_exists('nocaptcha_login_recaptcha_get_post')) {
     }
 }
 
+if (!function_exists('nocaptcha_login_recaptcha_process')) {
+function nocaptcha_login_recaptcha_process() {
+    if (array() == $_POST) {
+        return true;
+    }
+
+    $opt = get_option('nocaptcha_login_recaptcha_options');
+    $parameters = array(
+        'secret' => $opt['secret_key'],
+        'response' => nocaptcha_login_recaptcha_get_post('g-recaptcha-response'),
+        'remoteip' => nocaptcha_login_recaptcha_get_ip()
+    );
+    $url = 'https://www.google.com/recaptcha/api/siteverify?' . http_build_query($parameters);
+
+    $response = nocaptcha_login_recaptcha_open_url($url);
+    $json_response = json_decode($response, true);
+
+    if (!empty($opt['secret_key']) && isset($json_response['success']) && true !== $json_response['success']) {
+        header('Location: wp-login.php?login_recaptcha_err=1');
+        exit();
+    }
+}
+}
+
+if($nocaptcha_opts["login"]=="1") {
+    add_action('wp_authenticate', 'nocaptcha_login_recaptcha_process', 1);
+}
+
+if (!function_exists('nocaptcha_login_recaptcha_open_url')) {
+function nocaptcha_login_recaptcha_open_url($url) {
+    if (function_exists('curl_init') && function_exists('curl_setopt') && function_exists('curl_exec')) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $response = curl_exec($ch);
+        curl_close($ch);
+    } else {
+        $response = file_get_contents($url);
+    }
+    return trim($response);
+}
+}
+
+function login_style_fix() {
+
+    $wowinitialize = ' <style type="text/css">
+                        #login {
+                             width: 350px;
+                                }
+                        .g-recaptcha {
+                          margin-bottom: 10px;
+                        }
+                        </style>';
+    echo $wowinitialize;
+
+}
+if($nocaptcha_opts["login"]=="1") {
+    add_action('login_head', 'login_style_fix');
+}
 
 
+/* Registration Page */
+if (!function_exists('nocaptcha_login_recaptcha_reg_process')) {
 
-
-
-
-    function nocaptcha_login_recaptcha_process() {
+    function nocaptcha_login_recaptcha_reg_process()
+    {
         if (array() == $_POST) {
             return true;
         }
@@ -133,46 +243,148 @@ if (!function_exists('nocaptcha_login_recaptcha_get_post')) {
         $json_response = json_decode($response, true);
 
         if (!empty($opt['secret_key']) && isset($json_response['success']) && true !== $json_response['success']) {
-            header('Location: wp-login.php?login_recaptcha_err=1');
+            header('Location: wp-login.php?action=register&login_recaptcha_err=1');
             exit();
         }
     }
-
-    add_action('wp_authenticate', 'nocaptcha_login_recaptcha_process', 1);
-
-
-
-    function nocaptcha_login_recaptcha_open_url($url) {
-        if (function_exists('curl_init') && function_exists('curl_setopt') && function_exists('curl_exec')) {
-            $ch = curl_init();
-            curl_setopt($ch, CURLOPT_URL, $url);
-            curl_setopt($ch, CURLOPT_HEADER, false);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_TIMEOUT, 60);
-            curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
-            $response = curl_exec($ch);
-            curl_close($ch);
-        } else {
-            $response = file_get_contents($url);
-        }
-        return trim($response);
-    }
-
-function login_style_fix() {
-
-    $wowinitialize = ' <style type="text/css">
-                        #login {
-                             width: 350px;
-                                }
-                        .g-recaptcha {
-                          margin-bottom: 10px;
-                        }
-                        </style>';
-    echo $wowinitialize;
+}
+if($nocaptcha_opts["register"]=="1") {
+    add_action('register_form', 'nocaptcha_login_recaptcha_form');
+    add_action('register_post', 'nocaptcha_login_recaptcha_reg_process', 10, 3);
 
 }
-add_action('login_head', 'login_style_fix');
+
+if($nocaptcha_opts["comments"]=="1") {
+    add_action( 'comment_form_after_fields', 'nocaptcha_comment_form');
+    add_action( 'comment_form_logged_in_after', 'nocaptcha_comment_form');
+
+}
+
+if (!function_exists('nocaptcha_comment_form')) {
+
+function nocaptcha_comment_form() {
 
 
+
+    if ( is_user_logged_in()) {
+        return true;
+    }
+
+    $opt = get_option('nocaptcha_login_recaptcha_options');
+
+    $captcha_code = '';
+    if ('' != $opt['site_key'] && '' != $opt['secret_key']) {
+        $captcha_code .= '<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+			<div class="g-recaptcha" data-sitekey="'.htmlentities($opt['site_key']).'"></div>';
+        if (1 == $login_recaptcha_err) {
+            $captcha_code .= '<div style="color:#FF7425;">Human verification failed!</div>';
+        }
+    }
+    echo $captcha_code;
+
+    return true;
+}
+
+}
+if($nocaptcha_opts["comments"]=="1") {
+    add_filter('preprocess_comment', 'cptch_comment_post');
+}
+
+if ( ! function_exists( 'cptch_comment_post' ) ) {
+    function cptch_comment_post( $comment ) {
+
+
+        if ( is_user_logged_in() ) {
+            return $comment;
+        }
+
+        if ( isset( $_REQUEST['action'] ) && 'replyto-comment' == $_REQUEST['action'] &&
+            ( check_ajax_referer( 'replyto-comment', '_ajax_nonce', false ) || check_ajax_referer( 'replyto-comment', '_ajax_nonce-replyto-comment', false ) ) ) {
+            return $comment;
+        }
+
+        if ( '' != $comment['comment_type'] && 'comment' != $comment['comment_type'] ) {
+            return $comment;
+        }
+
+        $opt = get_option('nocaptcha_login_recaptcha_options');
+        $parameters = array(
+            'secret' => $opt['secret_key'],
+            'response' => nocaptcha_login_recaptcha_get_post('g-recaptcha-response'),
+            'remoteip' => nocaptcha_login_recaptcha_get_ip()
+        );
+        $url = 'https://www.google.com/recaptcha/api/siteverify?' . http_build_query($parameters);
+
+        $response = nocaptcha_login_recaptcha_open_url($url);
+        $json_response = json_decode($response, true);
+
+        if (!empty($opt['secret_key']) && isset($json_response['success']) && true !== $json_response['success']) {
+            wp_die( __('Human verification failed', 'captcha' ) );
+        }else{
+            return( $comment );
+        }
+
+    }
+}
+
+/* buddypress */
+if ( ! function_exists( 'bp_add_recaptcha' ) ) {
+
+    function bp_add_recaptcha()
+    {
+        global $bp;
+
+
+        $opt = get_option('nocaptcha_login_recaptcha_options');
+
+        $captcha_code = '';
+        if ('' != $opt['site_key'] && '' != $opt['secret_key']) {
+            $captcha_code .= '<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+			<div class="g-recaptcha" data-sitekey="' . htmlentities($opt['site_key']) . '"></div>';
+        }
+
+        $html = '<div class="register-section" id="nocaptcha" style="float: right;">';
+        $html .= '<div class="editfield">';
+        $html .= '<label>Are you robot?</label>';
+        if (!empty($bp->signup->errors['recaptcha_response_field'])) {
+            $html .= '<div class="error">';
+            $html .= $bp->signup->errors['recaptcha_response_field'];
+            $html .= '</div>';
+        }
+        $html .= $captcha_code;
+        $html .= '</div>';
+        $html .= '</div>';
+        echo $html;
+    }
+}
+if ( ! function_exists( 'bp_validate_recaptcha' ) ) {
+
+    function bp_validate_recaptcha($errors)
+    {
+        global $bp, $strError;
+
+        $opt = get_option('nocaptcha_login_recaptcha_options');
+        $parameters = array(
+            'secret' => $opt['secret_key'],
+            'response' => nocaptcha_login_recaptcha_get_post('g-recaptcha-response'),
+            'remoteip' => nocaptcha_login_recaptcha_get_ip()
+        );
+        $url = 'https://www.google.com/recaptcha/api/siteverify?' . http_build_query($parameters);
+
+        $response = nocaptcha_login_recaptcha_open_url($url);
+        $json_response = json_decode($response, true);
+
+        if (!empty($opt['secret_key']) && isset($json_response['success']) && true !== $json_response['success']) {
+            $bp->signup->errors['recaptcha_response_field'] = '<div style="color:#FF7425;">Human verification failed!</div>';
+        }
+        return;
+    }
+}
+
+if($nocaptcha_opts["buddypress"]=="1") {
+
+    add_action('bp_before_registration_submit_buttons', 'bp_add_recaptcha');
+    add_action('bp_signup_validate', 'bp_validate_recaptcha');
+}
 
 ?>
--- readme.txt
+++ readme.txt
@@ -1,26 +1,31 @@
 === Are you robot google recaptcha for wordpress ===
 Contributors: sureshdsk
 Donate link: http://www.idiotinside.com/contribution/
-Tags: recaptcha,google no captcha,are you robot,wp-login,login screen ,custom wplogin page
+Tags: recaptcha,google no captcha,are you robot,wp-login,registration,comments, spam, buddy press
 Requires at least: 3.0
 Tested up to: 4.0.1
 Stable tag: 4.0.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
+Adds the new google “Are you robot?” recaptcha to wp-login page, registration page, comment forms and buddypress registration page.
+
+== Description ==
+
 Adds the new google “Are you robot?” recaptcha to wp-login page.
 
 == Description ==
-Use this plugin to add the new google “Are you robot?” recaptcha to wp-login page. <br />
-Get the public key and private key from  https://www.google.com/recaptcha/admin and enter the keys in plugin settings page.
+Use this plugin to add the new google “Are you robot?” recaptcha to wp-login page, registration page, comment forms and buddypress registration page. <br />
+Get the public key and private key from  https://www.google.com/recaptcha/admin and enter the keys in plugin settings page and enable the recaptcha.
 <br />
 Plugin page: http://www.idiotinside.com/are-you-robot-google-recaptcha-for-wordpress/ <br />
 <br />
-Follow us :<br />
+`Follow us :`<br />
 <a href="http://facebook.com/idiotinside/">facebook</a> <br />
 <a href="https://twitter.com/IdiotInside_">twitter</a> <br />
 <a href="https://plus.google.com/106331226863845057962">google+</a><br />
 <a href="http://www.idiotinside.com/">IdiotInside.com</a> 
+
 
 == Installation ==
 
@@ -28,26 +33,35 @@ Follow us :<br />
 1. Upload plugin folder to the /wp-content/plugins/ directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
 
-
-
 == Frequently Asked Questions ==
-<b> How to use? </b>
+
+= How to use? =
+
 Register your site in google recaptcha and enter private key and public key in the plugin settings page. <br />
 See plugin settings page for more instruction.
 
 == Screenshots ==
 
-1. screenshot1.png 
-2. screenshot2.png
-
+1. screenshot-1.png
+2. screenshot-2.png
 
 == Changelog ==
 
 = 1.0 =
-* Initial release
+* A change since the previous version.
+* Another change.
 
+= 0.5 =
+* List versions from most recent at top to oldest at bottom.
 
 == Upgrade Notice ==
+
+= 2.0 =
+Added captcha on
+* wp registration page
+* comments form 
+* buddypress registration page
+
 = 1.0 =
-Initial release.
+* Initial release
 
